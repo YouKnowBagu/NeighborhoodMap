5c2c2e9b3d78cdea559e236e2e4eed7a4e3dd7be
Sean Sampson <sean.p.sampson@gmail.com>
2017-01-16 21:43:24 -0700

    Implement basic search filter

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,82 +1,131 @@
 // I went through quite a few iterations of this.  At first I wanted to add the ability for users to add markers to the map that would then display FourSquare location data.  I nearly had this working, but for places with multiple locations (e.g. McDonald's, Starbucks, etc), each marker would display EVERY locatoin's infoWindow, stacked on top of each other.  I spent some time trying to work that out but decided to focus on getting the MVP working.
 'use strict';
-var testItems = [
-	{
-		name: 'Utah State Capitol',
-		lat: 40.777,
-		lng: -111.888
-	},
-	{
-		name: "Red Butte Trail",
-		lat: 40.7659,
-		lng: -111.8225
-	}
+// var testVenues = [
+// 	{
+// 		Name: 'Utah State Capitol',
+// 		address: "Test address street road",
+// 		url: "www.website.com",
+// 		number:"404-404-4404"
+// 	},
+// 	{
+// 		Name: "Red Butte Trail",
+// 		address: "Test address street road",
+// 		url: "www.website.com",
+// 		number:"404-404-4404"
+// 	},
+// 	{
+// 		Name:"Sean's house",
+// 		address:"Sean's address",
+// 		url: "seanpsampson.com",
+// 		number: "404-404-4404"
+// 	}];
 //FourSquare keys
-var	clientID = "LNIAGQRXCOHRU0NUYS50R2KQC1SVITFKR1YRFXQ0XC5DOSHZ";
-var	clientSecret = "1VN1YABW1IHLXRJKQSWZ5KWVRMJVSKQWR35IBDXSIITCXEZV";
+// var	clientID = "LNIAGQRXCOHRU0NUYS50R2KQC1SVITFKR1YRFXQ0XC5DOSHZ";
+// var	clientSecret = "1VN1YABW1IHLXRJKQSWZ5KWVRMJVSKQWR35IBDXSIITCXEZV";
 //Global var def
 var map;
 
 //Venue Model
-var Venue = function(data) {
-	var self = this;
+// var viewModel = {
+// 		venues:
+// 		[{
+// 			Name: 'Utah State Capitol',
+// 			address: "Test address street road",
+// 			url: "www.website.com",
+// 			number:"404-404-4404"
+// 		},
+// 		{
+// 			Name: "Red Butte Trail",
+// 			address: "Test address street road",
+// 			url: "www.website.com",
+// 			number:"404-404-4404"
+// 		},
+// 		{
+// 			Name:"Sean's house",
+// 			address:"Sean's address",
+// 			url: "seanpsampson.com",
+// 			number: "404-404-4404"
+// 		}]
+// 	};
+	// var self = this;
     //Stuff here
 
 
-     var fourSquareUrl = //TODO Get URL JSON Query
-	$.getJSON(fourSquareUrl).done(function(data) {
-		var results = data.response.venues[0];
-
-		}
-	}).fail(function() {
-		alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
-	});
-
-	//TODO Determine InfoWindow content
-	this.content
-
-	this.infoWindow = new google.maps.InfoWindow({content: self.content});
-
-	this.marker = new google.maps.Marker({
-			//Marker Stuff here.  Name, position, etc.
-	});
-
-
-
-	this.marker.addListener('click', function(){
-		//infoWindow content stuff here
-		,
-
-        self.infoWindow.setContent(self.content);
-
-		self.infoWindow.open(map, this);
+    //  var fourSquareUrl = //TODO Get URL JSON Query
+	// $.getJSON(fourSquareUrl).done(function(data) {
+	// 	var results = data.response.venues[0];
+	//
+	// 	}
+	// }).fail(function() {
+	// 	alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
+	// });
+	//
+	// //TODO Determine InfoWindow content
+	// this.content
+	//
+	// this.infoWindow = new google.maps.InfoWindow({content: self.content});
+	//
+	// this.marker = new google.maps.Marker({
+	// 		//Marker Stuff here.  Name, position, etc.
+	// });
 
 
-	});
+
+	// this.marker.addListener('click', function(){
+	// 	//infoWindow content stuff here
+	// 	,
+	//
+    //     self.infoWindow.setContent(self.content);
+	//
+	// 	self.infoWindow.open(map, this);
+	//
+	//
+	// });
+// };
+// var ViewModel = function() {
+// 	var self = this;
+// 	this.searchBox = ko.observable("");
+// 	this.venues = ko.observableArray(venues);
+// 	//Initialize map centered on Utah Capitol
+    // map = new google.maps.Map(document.getElementById('map'), {
+    //         center: {
+    //             lat: 40.777,
+    //             lng: -111.888
+    //         },
+    //         zoom: 14
+    //     });
+// 			testItems.forEach(function(testItem){
+// 				self.testArray.push( new Venue(testItem));
+// 			});
+//
+// //Beginnings of search filter
+// 	this.filtering = ko.computed( function() {
+//
+// 	//Grab Map Element
+	// this.mapElem = document.getElementById('map');
+// 	this.mapElem.style.height = window.innerHeight - 50;
+//     }
+var viewModel = {
+  items: [ { Name: "Burger" }, { Name: "Fries" }, { Name: "Apple" }, { Name: "Salad" }, { Name: "Potatoes" }, { Name: "Mac n' Cheese" } ]
 };
-var viewModel = function() {
-	var self = this;
-	this.searchBox = ko.observable("");
-	this.testArray = ko.observableArray([]);
-	//Initialize map centered on Utah Capitol
-    map = new google.maps.Map(document.getElementById('map'), {
-            center: {
-                lat: 40.777,
-                lng: -111.888
-            },
-            zoom: 14
-        });
-			testItems.forEach(function(testItem){
-				self.testArray.push( new Venue(testItem));
-			});
 
-//Beginnings of search filter
-	this.filtering = ko.computed( function() {
+viewModel.Query = ko.observable('');
 
-	//Grab Map Element
-	this.mapElem = document.getElementById('map');
-	this.mapElem.style.height = window.innerHeight - 50;
-    }
+viewModel.searchResults = ko.computed(function() {
+    var q = viewModel.Query().toLowerCase();
+    return viewModel.items.filter(function(i) {
+      return i.Name.toLowerCase().indexOf(q) >= 0;
+    });
+  });
 
-//Start knockout
-ko.applyBindings(new viewModel());
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: 40.777,
+                    lng: -111.888
+                },
+                zoom: 14
+            });
+};
+
+ko.applyBindings(viewModel);
